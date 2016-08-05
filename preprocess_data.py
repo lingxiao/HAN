72091c4ce0e63433305296422f75aa6d7428d6a2
Xiao Ling <lingxiao@seas.upenn.edu>
2016-08-04 21:11:55 -0400

    better docs

--- preprocess_data.py
+++ preprocess_data.py
@@ -26,9 +26,10 @@ tok  = Tokenizer()
 #   Some common cleaning routines (effectful) 
 # ---------------------------------------------------------------------------------------------------
 
-# Given valid file path `inFile` and output file names
-# `outX` for the set of regressors and `outy` for set of regressands,
-# read and clean yelp jsn according to subroutine `yelp`.
+# @Use: Given valid file path `inFile` and output file names
+#       `outX` for the set of regressors and `outy` for set of regressands,
+#       clean and write yelp reviews and stars according to subroutine `yelp`.
+
 # doYelp :: String -> String -> String -> ReaderT _ IO Bool
 def doYelp (inPath, outX, outy):
     goXy (yelp, inPath, outX, outy)
@@ -37,7 +38,10 @@ def doYelp (inPath, outX, outy):
 #   Same common clean routines as above but effect-free
 # ---------------------------------------------------------------------------------------------------
 
-# Logic specific to yelp json with [text] and [stars] fields
+# @Use: Given a line of JSON object in string from `xs`
+#       so that `xs` has field `text` and `stars`, pull out and scrub the 
+#       `text` as X and `stars` as y, return a tuple.
+
 # yelp :: String -> (Unicode, Unicode)
 def yelp (xs):
     xs = toUnicode (xs)
@@ -47,11 +51,11 @@ def yelp (xs):
     return (X,y)
 
 
-# core process words logic:
-# 1. remove white space
-# 2. all lower case
-# 3. make all numbers in <NUM>
-# 4. tokenize according to tworkenize
+# @Use: core process words logic:
+#       1. remove white space
+#       2. all lower case
+#       3. make all numbers in <NUM>
+#       4. tokenize according to tworkenize
 
 # scrub :: String -> Unicode
 def scrub (xs):
@@ -64,21 +68,15 @@ def scrub (xs):
     ys  = ' '.join(ys)              
     ys  = ys.encode('utf-8')
 
-    # if isinstance(ys, str):                
-        # ys  = ys.encode('utf-8')
     return ys
 
 # ---------------------------------------------------------------------------------------------------
 #   Main IO loops to read, scrub, and write to disk
 # ---------------------------------------------------------------------------------------------------
 
-# @Use: iterate over a file and pull out the `X` to be saved as `outName1`
-#       and the `b` 
-
-# Given valid file path `inFile` and output file name 'outName' 
-# and normalizing/tokenizing routine `clean`, 
-# read in file `xss` and `scrub` it, then write `yys` to local directory
-# return [True] if succssful, [False] otherwise
+# @Use: iterate over a file and extract and `clean` the `X` to be saved in file named `outX`
+#       and `y` to be saved in file named `outy`
+#       return true if input directory specified in `inPath` is valid, false otherwise
 
 # goXy :: (string -> (Unicode, Unicode)) 
 #         -> String -> String -> String 
@@ -101,8 +99,9 @@ def goXy (clean, inPath, outX, outy)
 #   Utils
 # ---------------------------------------------------------------------------------------------------
 
-# TOOD: honor the forall quantifier so that it's not a wat??
 
+# @Use: given `xs` of arbitrary type, convert it to unicode
+# TOOD: honor the forall quantifier so that it's not a wat??
 # toUnicode :: forall a. a -> Unicode
 def toUnicode (xs):
     if isinstance(xs, str):
